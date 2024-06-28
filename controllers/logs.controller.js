7f6fcd62f400eeee64570033e592fc46a94ef3b9
antonio <antonio.fr.martinezc@hotmail.com>
2024-06-27 22:29:59 -0400

    implemented updating and deleting

--- controllers/logs.controller.js
+++ controllers/logs.controller.js
@@ -25,3 +25,30 @@ logs.post("/", (req, res) => {
 	res.json(logsData[logsData.length - 1]);
 });
 module.exports = logs;
+
+//update
+
+logs.put("/:id", (req, res) => {
+	const { id } = req.params;
+	let logIndex = logsData.findIndex((log) => log.id === Number(id));
+
+	if (logIndex !== -1) {
+		logsData[logIndex] = { ...logsData[logIndex], ...req.body };
+		res.status(200).json(logsData[logIndex]);
+	} else {
+		res.status(404).json({ msg: `Log with id: ${id} not found` });
+	}
+});
+
+//destroy
+
+logs.delete("/:id", (req, res) => {
+	const { id } = req.params;
+	let logIndex = logsData.findIndex((log) => log.id === Number(id));
+	if (logIndex !== -1) {
+		logsData.splice(logIndex, 1);
+		res.status(200).json({ msg: `Log with id: ${id} successfully deleted` });
+	} else {
+		res.status(404).json({ msg: `Log with id: ${id} not found` });
+	}
+});
