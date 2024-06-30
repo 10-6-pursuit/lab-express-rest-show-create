c166e000ec5a0ad80d675b2b7c51f8004afe6a6f
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-06-30 14:24:20 -0400

    delete method working

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -2,20 +2,30 @@ const express = require("express");
 const logs = express.Router();
 const logsArray = require("../models/log");
 
+
+//INDEX
 logs.get("/", (req, res) => {
     res.status(200).send(logsArray);
 });
 
+//CREATE
 logs.post("/", (req, res) => {
     const incomingLog = { id: logsArray.length + 1, ...req.body};
     logsArray.push(incomingLog);
     res.status(201).send(logsArray[logsArray.length - 1]);
 });
 
-// workout.post("/", (req, res) => {
-//     const currentWorkout = {id: workoutArray.length + 1, ...req.body};
-//     workoutArray.push(currentWorkout);
-//     res.status(201).send(workoutArray[workoutArray.length - 1]);
-// });
+//DELETE
+
+logs.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    const indexToDelete = logsArray.findIndex(log => log.id === +id);
+    if(indexToDelete !== -1){
+        logsArray.splice(indexToDelete, 1);
+        res.redirect("/logs");
+    } else {
+        res.status(404).send({error404: `${id} Not Found`});
+    }
+})
 
 module.exports = logs;
