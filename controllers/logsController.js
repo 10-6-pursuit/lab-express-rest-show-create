203f6f6fd280e168eb1aea3c29dfa44795ec07c7
MarcusB86 <marcusb08@gmail.com>
2024-06-30 21:04:57 -0400

    all index routes passed tests

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -6,20 +6,37 @@ logs.get("/", (req,res) => {
     res.json(logsArray);
 })
 
-logs.get("/:id", (req, res) => {
-    const { id } = req.params;
-    const log = logsArray.find((log) => log.id === Number(id));
+logs.get("/:arrayIndex", (req, res) => {
+    const { arrayIndex } = req.params;
+    const log = logsArray[arrayIndex]           // ((log) => log.id === Number(id));
     if (log) {
       res.send(log);
     } else {
-      res.send("Yo my man, we can't find this id: " + id);
+      res.redirect("./logs");
     }
   });
 
+  
+
 logs.post("/", (req, res) => {
-    const newLog = { ...req.body, id: logsArray.length + 1};
-    logsArray.push(newLog);
+    const currentLog = { ...req.body};
+    logsArray.push(currentLog);
     res.json(logsArray[logsArray.length - 1]);
 })
+
+logs.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    const deletedLogIndex = logsArray.findIndex(
+      (log) => log.id === Number(id)
+    );
+    if (deletedLogIndex !== -1) {
+      const deletedColor = logsArray.splice(deletedLogIndex, 1);
+      res.redirect("/logs");
+    } else {
+      res.status(404).json({ error: "Not Found" });
+    }
+  });
+
+  
 
 module.exports = logs;
